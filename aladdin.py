d7b801b0f7b10697cc53efa61cd9bee257c27621
heinminchit73-svg <heinminchit73@gmail.com>
2026-07-18 15:08:46 +0630

    Update aladdin.py

--- aladdin.py
+++ aladdin.py
@@ -40,5 +40,5 @@ ALD-GMIYQHWFX1ZH | days | 20 Days
 ALD-OKDHWPFUXZ52 | very | 30 Days
 ALD-IMTHNCP3XWTI | ands | 30 Days
 ALD-C5XSF160SWOR | myokyaw | 1 Days
-ALD-JJ9LBQQ | name | 10
 ALD-M64FUYW | have | 10
+ALD-3I4V1C8 | namm | 1
